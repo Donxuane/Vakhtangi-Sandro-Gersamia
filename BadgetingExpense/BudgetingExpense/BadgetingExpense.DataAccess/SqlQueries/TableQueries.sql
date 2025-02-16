b795b3cb23c60cd8d0e9cef6b219ff78ff3907b0
Donxuane <vaxtangichitashvili@gmail.com>
2025-02-16 20:18:27 +0300

    updated sql Query

--- BadgetingExpense/BudgetingExpense/BadgetingExpense.DataAccess/SqlQueries/TableQueries.sql
+++ BadgetingExpense/BudgetingExpense/BadgetingExpense.DataAccess/SqlQueries/TableQueries.sql
@@ -2,15 +2,14 @@ Create Table UserIncome(
 Id int Primary Key Identity (1,1),
 Currency nvarchar(10) Not Null,
 IncomeType nvarchar(20) Not Null,
-UserId int
-Constraint Fk_UserIncome_UserMainId Foreign Key (UserId) References [User](Id) On Delete Cascade
+UserId nvarchar(450) Not Null 
 );
 
 Create Table IncomeDetails(
 Id int Primary Key Identity(1,1),
 IncomeAmount decimal(18,2) Not Null,
 IncomeDate date Not Null,
-UserIncomeId int
+UserIncomeId int Not Null
 Constraint Fk_IncomeDetails_UserIncomeId Foreign Key (UserIncomeId) References UserIncome(Id) On Delete Cascade
 );			
 
@@ -18,25 +17,23 @@ Create Table UserExpense(
 Id int Primary key Identity(1,1),
 Currency nvarchar(10) Not Null,
 ExpenseType nvarchar(50) Not Null,
-UserId int
-Constraint Fk_UserExpenses_UserMainId Foreign Key (UserId) References [User](Id) On Delete Cascade
+UserId nvarchar(450) Not Null
 );
 
 Create Table ExpenseDetails(
 Id int Primary Key Identity(1,1),
 ExpenseAmount decimal(18,2) Not Null,
 ExpenseDate date Not Null,
-UserExpenseId int
+UserExpenseId int Not Null
 Constraint Fk_ExpenseDetails_UserExpenseId Foreign Key (UserExpenseId) References UserExpense(Id) On Delete Cascade
 );
 
 Create Table ExpenseLimits(
-Id int Primary Key,
-UserId int Not Null,
+Id int Primary Key Identity(1,1),
+UserId nvarchar(450) Not Null,
 LimitPeriod nvarchar(10) Not Null,
 LimitExpenseType nvarchar(50),
 LimitAmount decimal(18,2) Not Null,
-Constraint FK_ExpenseLimits_UserId Foreign Key (UserId) References [User](Id) On Delete Cascade
 );
 
 Alter Table UserExpense
