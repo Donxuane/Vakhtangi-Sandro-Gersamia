1468033dc4923fe58cf2b31ef3d507cff67ad7e0
Donxuane <vaxtangichitashvili@gmail.com>
2025-02-27 20:37:54 +0300

    Added DataBase View For Income Reports

---
+++ BadgetingExpense/BudgetingExpense/BudgetingExpense.DataAccess/SqlQueries/ViewQueries.sql
@@ -0,0 +1,9 @@
+﻿Create View IncomeCategories As
+Select 
+i.UserId As UserId,
+i.Date As IncomeDate,
+i.Amount, 
+i.Currency,
+Coalesce(NullIf(c.Name, ''),'General') As CategoryName
+From Incomes i
+Left Join Categories c On i.CategoryId = c.Id where c.Type = 1;
