c1fcdf46ac06b40a2d8777d99d704c37f754de10
Donxuane <vaxtangichitashvili@gmail.com>
2025-03-14 21:41:01 +0300

    Configured IHostedService For Database Setup While Lunching Application

--- BadgetingExpense/BudgetingExpense/BudgetingExpense.DataAccess/SqlQueries/TableQueries.sql
+++ BadgetingExpense/BudgetingExpense/BudgetingExpense.DataAccess/SqlQueries/TableQueries.sql
@@ -50,7 +50,6 @@ i.Currency,
 Coalesce(NullIf(c.Name, ''),'General') As CategoryName
 From Incomes i
 Left Join Categories c On i.CategoryId = c.Id where c.Type = 1;
-
 Go
 Create View ExpenseCategories As
 Select
@@ -61,7 +60,6 @@ ex.Date,
 Coalesce(NullIf(c.Name,''),'General') As CategoryName
 From Expenses ex
 Left Join Categories c on ex.CategoryId = c.Id where c.Type = 0;
-
 Go
 CREATE VIEW BudgetPlaning AS
 SELECT 
