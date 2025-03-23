771f9b3aee185616aa338a5a7d5aefc87f07225d
Donxuane <vaxtangichitashvili@gmail.com>
2025-03-23 21:31:12 +0300

    Added

---
+++ BadgetingExpense/BudgetingExpense/BudgetingExpense.Domain/CustomValidationAttributes/ValidateInputAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BudgetingExpense.Domain.CustomValidationAttributes;
+[AttributeUsage(AttributeTargets.Property, AllowMultiple =true, Inherited =false)]
+public class NormalizeInputAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if(value is string input)
+        {
+            var checkNumber = new Regex(@"\d");
+            if(checkNumber.IsMatch(input))
+            {
+                return new ValidationResult("Avoid numbers!");
+            }
+            return ValidationResult.Success;
+        }
+        return new ValidationResult("Input value does not match!");
+    }
+}
