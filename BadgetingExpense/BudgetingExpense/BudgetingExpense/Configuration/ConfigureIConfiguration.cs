c773b910fd16d7130a7c371902186c5a77c50380
Donxuane <vaxtangichitashvili@gmail.com>
2025-03-23 17:34:08 +0300

    Added Json Folder

--- BadgetingExpense/BudgetingExpense/BudgetingExpense/Configuration/ConfigureIConfiguration.cs
+++ BadgetingExpense/BudgetingExpense/BudgetingExpense/Configuration/ConfigureIConfiguration.cs
@@ -5,7 +5,7 @@ public static class ConfigureIConfiguration
     public static void AddMultipleJsonFileConfiguration(this WebApplicationBuilder builder)
     {
         builder.Configuration
-            .SetBasePath(Directory.GetCurrentDirectory())
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "AppSettingsJson"))
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile("seedData.json", optional: true, reloadOnChange: true)
             .AddJsonFile("emailSettings.json", optional: true, reloadOnChange: true)
