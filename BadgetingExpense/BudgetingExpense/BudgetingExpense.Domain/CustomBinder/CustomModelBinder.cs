d3a709679a6f539ef8344fd421e95f329c5d6cc7
Donxuane <vaxtangichitashvili@gmail.com>
2025-03-27 14:47:57 +0300

    Updated Seed Data Json

--- BadgetingExpense/BudgetingExpense/BudgetingExpense.Domain/CustomBinder/CustomModelBinder.cs
+++ BadgetingExpense/BudgetingExpense/BudgetingExpense.Domain/CustomBinder/CustomModelBinder.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
 
 namespace BudgetingExpense.Domain.CustomBinder;
 
@@ -7,12 +8,15 @@ public class CustomModelBinder<T> : IModelBinder where T : class, new()
     public Task BindModelAsync(ModelBindingContext bindingContext)
     {
         var model = new T();
+        bool check = false;
 
         foreach (var property in typeof(T).GetProperties())
-        {
+        { 
+            var propertyResult = bindingContext.ValueProvider.GetValue(property.Name);
+            var result = propertyResult.FirstOrDefault();
+            object? valueToSet = null;
             if (property.PropertyType == typeof(string) && property.CanWrite)
             {
-                var propertyResult = bindingContext.ValueProvider.GetValue(property.Name);
                 if (propertyResult.Length != 0)
                 {
                     if (!string.IsNullOrEmpty(result))
@@ -20,13 +24,24 @@ public class CustomModelBinder<T> : IModelBinder where T : class, new()
                         if (!result.Contains('@') && !property.Name.Equals("Password", StringComparison.OrdinalIgnoreCase)
                            && !property.Name.Equals("RepeatPassword", StringComparison.OrdinalIgnoreCase))
                         {
-                            result = result.Trim().ToLower();
+                            valueToSet = result.Trim().ToLower();
+                            check = true;
                         }
+                        
                     }
                 }
             }
+            else
+            {
+                valueToSet = Convert.ChangeType(result, property.PropertyType, CultureInfo.InvariantCulture);
+            }
+            property.SetValue(model, valueToSet);
         }
-        bindingContext.Result = ModelBindingResult.Success(model);
+        if (check == true)
+        {
+            bindingContext.Result = ModelBindingResult.Success(model);
+        }
+        check = false;
         return Task.CompletedTask;
     }
 }
