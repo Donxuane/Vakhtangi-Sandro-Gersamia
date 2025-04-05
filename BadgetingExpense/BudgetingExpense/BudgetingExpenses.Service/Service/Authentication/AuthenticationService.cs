9d498f6091ba1697721c15424200fe4e23c18973
Donxuane <vaxtangichitashvili@gmail.com>
2025-04-05 10:08:44 +0300

    Seperated Token Logic

--- BadgetingExpense/BudgetingExpense/BudgetingExpenses.Service/Service/Authentication/AuthenticationService.cs
+++ BadgetingExpense/BudgetingExpense/BudgetingExpenses.Service/Service/Authentication/AuthenticationService.cs
@@ -1,9 +1,7 @@
 ﻿using BudgetingExpense.Domain.Contracts.IUnitOfWork;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Text;
 using BudgetingExpense.Domain.Models.AuthenticationModels;
 using BudgetingExpense.Domain.Models.MainModels;
 using BudgetingExpense.Domain.Contracts.IServices.IAuthentication;
@@ -164,56 +162,6 @@ public class AuthenticationService : IAuthenticationService
         catch (Exception ex)
         {
             _logger.LogError("Register new user {ex}", ex.Message);
-            throw;
-        }
-    }
-    public Task<string>? GenerateJwtTokenAsync(string userId, string userRole)
-    {
-        try
-        {
-            var tokenConfiguration = _configuration.GetSection("Jwt");
-            var tokenKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfiguration["Key"]));
-
-            var claims = new[]
-            {
-                    new Claim(ClaimTypes.Name, userId),
-                    new Claim(ClaimTypes.Role, userRole)
-            };
-            var token = new JwtSecurityToken
-            (
-                issuer: tokenConfiguration["Issuer"],
-                audience: tokenConfiguration["Audience"],
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(Convert.ToInt32(tokenConfiguration["ExpiryMinutes"])),
-                signingCredentials: new SigningCredentials(tokenKey, SecurityAlgorithms.HmacSha256)
-            );
-            _logger.LogInformation("Token generated for {userId}", userId);
-            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError("Exception {ex}", ex.Message);
-            throw;
-        }
-    }
-
-    public Task<string> GenerateRefreshToken(string userId)
-    {
-        try
-        {
-            var tokenKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt")["Key"]));
-            var claim = new[] { new Claim(ClaimTypes.Name, userId) };
-            var token = new JwtSecurityToken
-            (
-                claims: claim,
-                signingCredentials: new SigningCredentials(tokenKey, SecurityAlgorithms.HmacSha256)
-            );
-            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
-          
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError("Exception ex:{ex}", ex.Message);
             throw;
         }
     }
